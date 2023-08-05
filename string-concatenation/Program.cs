8cf8278a1eb54abd2e7b85e8dcf7c2b1e4ed0359
ARBUCHELI <radio_seattle@hotmail.com>
2023-08-05 12:58:48 +0300

    string concatenation

---
+++ string-concatenation/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace StoryTime
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      // Declare the variables
+      string beginning = "I started to play the piano when I was 9 years old.";
+
+      string middle = "I was good in math and nusic, and I for that reason, I decided to go the university to study music, but ...";
+
+      string end = "finally, my love for math, and science was bigger, and I decided to become an Engineer in Electronics.";
+
+      // Concatenate the string and the variables
+      string story = beginning + " " + middle + " " + end + "!!!" + "\n";
+
+      // Print the story to the console 
+      Console.WriteLine(story);
+    }
+  }
+}
