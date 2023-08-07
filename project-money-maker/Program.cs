f3d71872cdbceafb2351ae707fcf29f2ea394b73
ARBUCHELI <radio_seattle@hotmail.com>
2023-08-07 14:06:14 +0300

    project money maker version 1

---
+++ project-money-maker/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace MoneyMaker
+{
+  class MainClass
+  {
+    public static void Main(string[] args)
+    {
+      Console.WriteLine("Welcome to Money Maker!");
+      Console.Write("Enter money to convert to coins: ");
+      string money = Console.ReadLine();
+      double numeric_money = Convert.ToDouble(money);
+      Console.WriteLine($"{numeric_money} cents is equal to...");
+    }
+  }
+}
