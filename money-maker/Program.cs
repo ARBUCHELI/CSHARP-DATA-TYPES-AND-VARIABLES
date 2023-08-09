c9d375a9d68ed4b5a62d36fdfe7c85e11238ab86
ARBUCHELI <radio_seattle@hotmail.com>
2023-08-09 09:47:35 +0300

    project money maker

---
+++ money-maker/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace MoneyMaker
+{
+  class MainClass
+  {
+    public static void Main(string[] args)
+    {
+      Console.WriteLine("Welcome to Money Maker!");
+      Console.Write("Enter money to convert to coins: ");
+      string money = Console.ReadLine();
+      double numeric_money = Convert.ToDouble(money);
+      Console.WriteLine($"{numeric_money} cents is equal to...\n");
+      int goldValue = 10;
+      int silverValue = 5;
+      double goldCoins = Math.Floor(numeric_money /   goldValue);
+      double remainder = numeric_money % goldValue;
+      double silverCoins = Math.Floor(remainder /  silverValue);
+      remainder = remainder % silverValue;
+      Console.WriteLine($"Gold coins: {goldCoins}\n");
+      Console.WriteLine($"Silver coins: {silverCoins}\n");
+      Console.WriteLine($"Bronze coins: {remainder}\n");
+    }
+  }
+}
